8fb07392403e3c526366d8193d5d26e7442a5f70
Pbgreerb <pbgreerb@gmail.com>
2016-08-20 13:04:54 -0700

    Better client server sync
    So long as both client and host have a directory next to their
    executable named Card_Sprites that contains png images for each card. If
    a client is missing images or their images are named incorrectly a
    generic missing data image will show for the front of the card.
    
    Images all get resized to card size. (1050 x 750 px) for now.
    
    The side of the card syncs correctly on client connection for all cards
    now.

--- Card Games/Assets/Scripts/Card.cs
+++ Card Games/Assets/Scripts/Card.cs
@@ -1,12 +1,14 @@
 ﻿using UnityEngine;
 using UnityEngine.Networking;
 using System.Collections;
+using System.IO;
 
 public class Card : NetworkBehaviour {
 
 	static private Player s_local_player = null;
 
-	public string m_name;
+	[SyncVar]
+	public string m_filename = "";
 	public Sprite m_front;
 	public Vector3 m_blowup_size;
 
@@ -14,9 +16,9 @@ public class Card : NetworkBehaviour {
 	[SyncVar]
 	private bool m_held;
 	[SyncVar]
-	private bool m_upright;
+	public bool m_upright = false;
 	private bool m_holder;
-	private bool m_blownup;
+	private bool m_blownup = false;
 	private SpriteRenderer m_sprite_component;
 	private Vector3 m_normal_size;
 
@@ -25,22 +27,54 @@ public class Card : NetworkBehaviour {
 	public event NoArgDelegate EventFlip;
 
 	void Start () {
-		EventFlip = new NoArgDelegate(FlipEvent);
-		m_normal_size = new Vector3 (1, 1, 1);
-		m_blowup_size = new Vector3 (3, 3, 1);
+		m_holder = false;
 		m_back = Resources.Load<Sprite> ("Card_Back");
-		m_upright = false;
-		m_blownup = false;
 		m_sprite_component = this.GetComponent<SpriteRenderer> ();
-		m_sprite_component.sprite = m_back;
-	}
-
-	void OnConnectedToServer () {
+		EventFlip = new NoArgDelegate (FlipEvent);
+		m_normal_size = new Vector3 (0.33f, 0.33f, 1);
+		m_blowup_size = new Vector3 (1, 1, 1);
+		if (isClient) {
+			if (File.Exists (Application.dataPath + "/../Card_Sprites/" + m_filename)) {
+				LoadFront ();
+			} else {
+				m_front = Resources.Load<Sprite> ("Missing_Data");
+			}
+		}
 		if (m_upright) {
 			m_sprite_component.sprite = m_front;
 		} else {
 			m_sprite_component.sprite = m_back;
 		}
+	}
+		
+	// <summary>
+	// Loads an image for the front of the card from the
+	// Card_Sprites directory based on the set m_filename.
+	// 
+	// If no image is found a generic missing image is displayed.
+	// </summary>
+	public void LoadFront () {
+		byte[] bytes = System.IO.File.ReadAllBytes (Application.dataPath + "/../Card_Sprites/" + m_filename);
+		Texture2D texture = new Texture2D (1, 1);
+		texture.LoadImage (bytes);
+
+		// This scales all card images to the standard card size
+
+		int width = 750;
+		int height = 1050;
+		Rect texture_rect = new Rect (0, 0, width, height);
+		texture.filterMode = FilterMode.Trilinear;
+		texture.Apply (true);
+		RenderTexture render = new RenderTexture(width, height, 32);
+		Graphics.SetRenderTarget(render);
+		GL.LoadPixelMatrix(0,1,1,0);
+		GL.Clear(true,true,new Color(0,0,0,0));
+		Graphics.DrawTexture(new Rect(0,0,1,1),texture);
+		texture.Resize (width, height);
+		texture.ReadPixels (texture_rect, 0, 0, true);
+		texture.Apply (true);
+
+		m_front = Sprite.Create (texture, new Rect (0, 0, texture.width, texture.height), new Vector2 (0.5f, 0.5f), 100);
 	}
 
 	static public void SetLocalPlayer () {
--- Card Games/Assets/Scripts/SpawnCards.cs
+++ Card Games/Assets/Scripts/SpawnCards.cs
@@ -3,23 +3,23 @@ using UnityEngine.Networking;
 using System.Collections;
 using System.IO;
 
+	/* <summary>
+	 * Spawns Cards based on images in the Card_Sprites directory. All players need
+	 * the same images and image names as the host.
+	 * </summary>*/
 public class SpawnCards : NetworkBehaviour {
 
 	public GameObject card;
 
 	void Start () {
 		DirectoryInfo card_folder = new DirectoryInfo (Application.dataPath + "/../Card_Sprites");
-		FileInfo[] png_files = card_folder.GetFiles ("*.png");
-		//FileInfo[] jpg_files = card_folder.GetFiles ("*.jpg");
+		FileInfo[] png_files = card_folder.GetFiles ("*.png"); // can use jpg files too
 
 		foreach (FileInfo file in png_files) {
 			GameObject new_card = (GameObject)Instantiate (card, transform.position, Quaternion.identity);
-			var bytes = System.IO.File.ReadAllBytes (Application.dataPath + "/../Card_Sprites/" + file.Name);
-			var tex = new Texture2D (1, 1);
-			tex.LoadImage (bytes);
 			Card card_component = new_card.GetComponent<Card> ();
-			card_component.m_front = Sprite.Create (tex, new Rect (0, 0, tex.width, tex.height), new Vector2 (0.5f, 0.5f), 100);
-			card_component.m_name = file.Name;
+			card_component.m_filename = file.Name;
+			card_component.LoadFront ();
 			NetworkServer.Spawn (new_card);
 		}
 	}
